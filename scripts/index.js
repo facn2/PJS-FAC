5ab17a0e643882eecc81da5ee6d5cb476ff62e26
harrydry <harrydry1996@gmail.com>
2017-07-18 19:00:12 +0300

    Pushing photograph

--- scripts/index.js
+++ scripts/index.js
@@ -1,19 +1,16 @@
 var cityTemp = document.querySelector('.cityTemp');
-
+var imageIcon = document.querySelector('.imageIcon');
+console.log(imageIcon);
 
 addListener('.inputSearchForm', 'submit', function(event){
   event.preventDefault();
   var url = 'http://api.apixu.com/v1/current.json?key=e4bf9ee029124be085f120321171807&q='+ event.target[0].value;
 
   fetch(url, function (response) {
-  var humidity = response.current.humidity;
-  var tempC = response.current.temp_c;
-  cityTemp.innerHTML = tempC;
+	  var sunIcon = response.current.condition.icon;
+	  var tempC = response.current.temp_c;
+
+	  cityTemp.innerHTML = tempC;
+		imageIcon.src = sunIcon;
   });
-
 });
-
-// response.current.hummidity // hummidity
-// response.current.temp_c // celcius temp
-// response.current.temp_f // fh temp
-// response.current.condition.icon // temp icon
