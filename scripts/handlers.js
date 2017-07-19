5d15e99e5aedac51a987cde1770cbba0449de561
harrydry <harrydry1996@gmail.com>
2017-07-19 14:06:23 +0300

    guardian api changes

--- scripts/handlers.js
+++ scripts/handlers.js
@@ -2,6 +2,21 @@
 
 function addListener(selector, eventName, callback) {
   document.querySelector(selector).addEventListener(eventName, callback)
+
+}
+
+function fetch(url, callback){
+  var xhr = new XMLHttpRequest();
+
+  xhr.addEventListener('load', function() {
+    if (xhr.status === 200) {
+      var response = JSON.parse(xhr.responseText);
+      return callback(response);
+    }
+  });
+
+  xhr.open('GET', url);
+  xhr.send();
 }
 
 function fetch(url, callback){
--- scripts/index.js
+++ scripts/index.js
@@ -1,19 +1,57 @@
-var cityTemp = document.querySelector('.cityTemp');
-var imageIcon = document.querySelector('.imageIcon');
+var publicFigure2012 = document.querySelector('.publicFigure2012');
+var publicFigure2013 = document.querySelector('.publicFigure2013');
+var publicFigure2014 = document.querySelector('.publicFigure2014');
+var publicFigure2015 = document.querySelector('.publicFigure2015');
+var publicFigure2016 = document.querySelector('.publicFigure2016');
+var publicFigure2017 = document.querySelector('.publicFigure2017');
 
 addListener('.inputSearchForm', 'submit', function(event){
   event.preventDefault();
-  var url = 'http://api.apixu.com/v1/current.json?key=e4bf9ee029124be085f120321171807&q='+ event.target[0].value;
+	var initialSearch = event.target[0].value;
+	console.log(initialSearch);
+	var updatedSearch = initialSearch.replace(' ', '%20');
+	console.log(updatedSearch);
 
-  fetch(url, function (response) {
-	  var sunIcon = response.current.condition.icon;
-	  var tempC = response.current.temp_c;
+	var url = 'http://content.guardianapis.com/search?from-date=2012-01-01&to-date=2012-12-31&q=' + updatedSearch + '&api-key=1b0f1dd2-eba1-46b2-b432-ba942efb90e5'
 
-	  cityTemp.innerHTML = tempC;
-		imageIcon.src = sunIcon;
-  });
+	var url2013 = 'http://content.guardianapis.com/search?from-date=2013-01-01&to-date=2013-12-31&q=' + updatedSearch + '&api-key=1b0f1dd2-eba1-46b2-b432-ba942efb90e5'
+
+	var url2014 = 'http://content.guardianapis.com/search?from-date=2014-01-01&to-date=2014-12-31&q=' + updatedSearch + '&api-key=1b0f1dd2-eba1-46b2-b432-ba942efb90e5'
+
+	var url2015 = 'http://content.guardianapis.com/search?from-date=2015-01-01&to-date=2015-12-31&q=' + updatedSearch + '&api-key=1b0f1dd2-eba1-46b2-b432-ba942efb90e5'
+
+	var url2016 = 'http://content.guardianapis.com/search?from-date=2016-01-01&to-date=2016-12-31&q=' + updatedSearch + '&api-key=1b0f1dd2-eba1-46b2-b432-ba942efb90e5'
+
+	var url2017 = 'http://content.guardianapis.com/search?from-date=2017-01-01&to-date=2017-12-31&q=' + updatedSearch + '&api-key=1b0f1dd2-eba1-46b2-b432-ba942efb90e5'
+
+fetch(url, function (response) {
+	console.log(response.response.total);
 });
 
- https://newsapi.org/v1/articles?source=techcrunch&apiKey=a5c68819c9574874bda2eb6683023e8f
-a5c68819c9574874bda2eb6683023e8f
+	fetch(url2013, function (response) {
+		console.log(response.response.total);
+});
+
+	fetch(url2014,function(response) {
+		console.log(response.response.total);
+});
+
+fetch(url2015, function (response) {
+	console.log(response.response.total);
+});
+
+	fetch(url2016, function (response) {
+		console.log(response.response.total);
+});
+
+	fetch(url2017,function(response) {
+		console.log(response.response.total);
+});
+
+});
+
+
+
+
+
 
