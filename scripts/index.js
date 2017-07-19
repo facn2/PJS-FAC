69a803783088eab86ab0a5d0fb5c0058cc59f77f
harrydry <harrydry1996@gmail.com>
2017-07-19 18:40:37 +0300

    Final Changes

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,6 @@
+var profileGifContainer = document.querySelector('.profileGifContainer');
+var noiseContainInner = document.querySelector('.noiseContainInner');
+
 var publicFigure2012 = document.querySelector('.publicFigure2012');
 var publicFigure2013 = document.querySelector('.publicFigure2013');
 var publicFigure2014 = document.querySelector('.publicFigure2014');
@@ -8,9 +11,7 @@ var publicFigure2017 = document.querySelector('.publicFigure2017');
 addListener('.inputSearchForm', 'submit', function(event){
   event.preventDefault();
 	var initialSearch = event.target[0].value;
-	console.log(initialSearch);
 	var updatedSearch = initialSearch.replace(' ', '%20');
-	console.log(updatedSearch);
 
 	var url = 'http://content.guardianapis.com/search?from-date=2012-01-01&to-date=2012-12-31&q=' + updatedSearch + '&api-key=1b0f1dd2-eba1-46b2-b432-ba942efb90e5'
 
@@ -24,30 +25,41 @@ addListener('.inputSearchForm', 'submit', function(event){
 
 	var url2017 = 'http://content.guardianapis.com/search?from-date=2017-01-01&to-date=2017-12-31&q=' + updatedSearch + '&api-key=1b0f1dd2-eba1-46b2-b432-ba942efb90e5'
 
+	var urlGiphy = 'http://api.giphy.com/v1/gifs/search?q=' + event.target[0].value + '&api_key=eb3ec09a06d34a58a7e16f62990f1c93&limit=12';
+
+
 fetch(url, function (response) {
-	console.log(response.response.total);
+	// console.log(response.response.total);
 });
 
 	fetch(url2013, function (response) {
-		console.log(response.response.total);
+		// console.log(response.response.total);
 });
 
 	fetch(url2014,function(response) {
-		console.log(response.response.total);
+		// console.log(response.response.total);
 });
 
 fetch(url2015, function (response) {
-	console.log(response.response.total);
+	// console.log(response.response.total);
 });
 
 	fetch(url2016, function (response) {
-		console.log(response.response.total);
+		// console.log(response.response.total);
 });
 
 	fetch(url2017,function(response) {
-		console.log(response.response.total);
+		// console.log(response.response.total);
 });
 
+fetch(urlGiphy, function(response) {
+		console.log(response);
+		var gifImage = response.data[0].images.downsized_medium.url;
+		noiseContainInner.style.backgroundImage = `url('${gifImage}')`
+});
+
+
+event.target.reset();
 });
 
 
