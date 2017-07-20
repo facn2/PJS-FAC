9cc70ba154ece9f85686c0c5dac808db4e836640
Paul Hanna <paulhanna1933@gmail.com>
2017-07-20 13:06:19 +0300

    Issue 24

--- scripts/index.js
+++ scripts/index.js
@@ -1,69 +1,43 @@
 var profileGifContainer = document.querySelector('.profileGifContainer');
 var noiseContainInner = document.querySelector('.noiseContainInner');
+var isFamous = document.querySelector('.isFamous');
 
-var publicFigure2012 = document.querySelector('.publicFigure2012');
-var publicFigure2013 = document.querySelector('.publicFigure2013');
-var publicFigure2014 = document.querySelector('.publicFigure2014');
-var publicFigure2015 = document.querySelector('.publicFigure2015');
-var publicFigure2016 = document.querySelector('.publicFigure2016');
-var publicFigure2017 = document.querySelector('.publicFigure2017');
 
 addListener('.inputSearchForm', 'submit', function(event){
   event.preventDefault();
 	var initialSearch = event.target[0].value;
 	var updatedSearch = initialSearch.replace(' ', '%20');
 
-	var url = 'http://content.guardianapis.com/search?from-date=2012-01-01&to-date=2012-12-31&q=' + updatedSearch + '&api-key=1b0f1dd2-eba1-46b2-b432-ba942efb90e5'
-
-	var url2013 = 'http://content.guardianapis.com/search?from-date=2013-01-01&to-date=2013-12-31&q=' + updatedSearch + '&api-key=1b0f1dd2-eba1-46b2-b432-ba942efb90e5'
-
-	var url2014 = 'http://content.guardianapis.com/search?from-date=2014-01-01&to-date=2014-12-31&q=' + updatedSearch + '&api-key=1b0f1dd2-eba1-46b2-b432-ba942efb90e5'
-
-	var url2015 = 'http://content.guardianapis.com/search?from-date=2015-01-01&to-date=2015-12-31&q=' + updatedSearch + '&api-key=1b0f1dd2-eba1-46b2-b432-ba942efb90e5'
-
-	var url2016 = 'http://content.guardianapis.com/search?from-date=2016-01-01&to-date=2016-12-31&q=' + updatedSearch + '&api-key=1b0f1dd2-eba1-46b2-b432-ba942efb90e5'
-
 	var url2017 = 'http://content.guardianapis.com/search?from-date=2017-01-01&to-date=2017-12-31&q=' + updatedSearch + '&api-key=1b0f1dd2-eba1-46b2-b432-ba942efb90e5'
 
 	var urlGiphy = 'http://api.giphy.com/v1/gifs/search?q=' + event.target[0].value + '&api_key=eb3ec09a06d34a58a7e16f62990f1c93&limit=12';
 
-
-fetch(url, function (response) {
-	// console.log(response.response.total);
-});
-
-	fetch(url2013, function (response) {
-		// console.log(response.response.total);
-});
-
-	fetch(url2014,function(response) {
-		// console.log(response.response.total);
-});
-
-fetch(url2015, function (response) {
-	// console.log(response.response.total);
-});
-
-	fetch(url2016, function (response) {
-		// console.log(response.response.total);
-});
-
 	fetch(url2017,function(response) {
-		// console.log(response.response.total);
+		var result2017 = (response.response.total);
+    if (result2017 < 100){
+      isFamous.innerHTML = `W<br>H<br>O<br><br> ?<br>?`;
+      noiseContainInner.style.backgroundImage = `url('https://media4.giphy.com/media/uHEqSttWHv476/giphy.gif')`
+
+    } else if (result2017 < 500) {
+      isFamous.textContent = 'Your almost there!!';
+
+    } else {
+      isFamous.textContent = 'You are super famous';
+    }
+
 });
+
+
+
+// var totalResult = result2012 + result2013 + result2014 + result2015 + result2016 + result2017;
+// console.log(totalResult);
+
 
 fetch(urlGiphy, function(response) {
-		console.log(response);
 		var gifImage = response.data[0].images.downsized_medium.url;
+    console.log(gifImage);
 		noiseContainInner.style.backgroundImage = `url('${gifImage}')`
 });
 
-
 event.target.reset();
 });
-
-
-
-
-
-
